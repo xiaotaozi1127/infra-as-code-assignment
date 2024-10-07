a50c2f3e2bf5bf01bb6720299101e3fa308c168e
xiaotaozi1127 <xiaotaozi1127@hotmail.com>
2024-10-07 16:47:14 +0800

    create dynamodb

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,8 +1,6 @@
 resource "aws_dynamodb_table" "user-info-table" {
   name           = format("%s-user-info-table", var.prefix)
   billing_mode   = "PAY_PER_REQUEST"
-  read_capacity  = 5
-  write_capacity = 5
   hash_key       = "UserId"
 
   attribute {
