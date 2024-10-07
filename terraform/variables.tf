b15dff2a885017aeecd1f2ccd8db3847827cf2e6
xiaotaozi1127 <xiaotaozi1127@hotmail.com>
2024-10-07 12:40:21 +0800

    update s3 terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,8 @@ variable "prefix" {
   type        = string
   description = "Prefix to many of the resources created which helps as an identifier, could be company name, solution name, etc"
 }
+
+variable "region" {
+  type        = string
+  description = "Region to deploy the solution"
+}
