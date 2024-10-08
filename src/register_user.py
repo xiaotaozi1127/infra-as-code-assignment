024667fdd93ac18a7f74ffc5e5fc855d63083b33
xiaotaozi1127 <xiaotaozi1127@hotmail.com>
2024-10-08 20:39:33 +0800

    update register user function

--- src/register_user.py
+++ src/register_user.py
@@ -1,19 +1,27 @@
-import boto3
 from os import getenv
-from urllib.parse import parse_qsl
+
+import boto3
 
 
 def lambda_handler(event, context):
-    # parse_qsl is used to parse a query string into a list of key-value pairs
-    # query_string = "param1=value1&param2=value2&param3=value3"
-    # [('param1', 'value1'), ('param2', 'value2'), ('param3', 'value3')]
-    print("rawQueryString for register user handler: ", event["rawQueryString"])
-    query_string = dict(parse_qsl(event["rawQueryString"]))
+    # "queryStringParameters":
+    #     {
+    #         "userid": "taohui"
+    #     }
+    print("queryStringParameters for register user: ", event["queryStringParameters"])
+    query_string = event["queryStringParameters"]
     client = boto3.resource("dynamodb")
     db_table = client.Table(getenv("DB_TABLE_NAME"))
     try:
-        response = db_table.put_item(Item=query_string)
-        return { "message": "Registered User Successfully" }
+        # Define the item you want to put into the table
+        # item = {
+        #     'YourPrimaryKey': 'YourPrimaryKeyValue',  # Replace with your primary key and value
+        #     'Attribute1': 'Value1',
+        #     'Attribute2': 'Value2',
+        #     # Add more attributes as needed
+        # }
+        db_table.put_item(Item=query_string)
+        return {"message": "Registered User Successfully"}
     except Exception as error_details:
         print(error_details)
-        return { "message": "Error registering user. Check Logs for more details." }
+        return {"message": "Error registering user. Check Logs for more details."}
