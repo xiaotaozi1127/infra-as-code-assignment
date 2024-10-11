0bc5038a42a359efe981fbe92c20fd49c6edf275
xiaotaozi1127 <xiaotaozi1127@hotmail.com>
2024-10-11 17:59:55 +0800

    fix stage issue

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -50,7 +50,6 @@ resource "aws_api_gateway_deployment" "deployment" {
   count       = length(var.functions)
   depends_on  = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.apis[count.index].id
-  stage_name  = var.stage_name
   triggers = {
     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.apis[count.index].body))
   }
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,6 +1,6 @@
 prefix     = "tw-infra-taohui"
 region     = "ap-southeast-2"
-stage_name = "dev"
+stage_name = "default"
 webpages = [
   {
     name = "index.html"
